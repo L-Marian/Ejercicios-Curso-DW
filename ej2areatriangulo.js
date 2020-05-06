1c93f53168a121edd071cfbd78057a367980f8cf
L-Marian <64810432+L-Marian@users.noreply.github.com>
2020-05-06 18:23:43 -0300

    Add files via upload

---
+++ ej2areatriangulo.js
@@ -0,0 +1,8 @@
+function calcularAreaTriangulo(base,altura) {
+	const superficie = base*altura
+	return superficie
+}
+const base = [5,8,10]
+const altura = [6,1,8]
+
+console.log(calcularAreaTriangulo(base[2],altura[0]))
